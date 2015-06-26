8a499eb3efac686b3f5a83860f1e837fc1f2dc65
René Schwaiger <sanssecours@f-m.fm>
2015-06-26 08:28:43 +0200

    Simplify code for exercise 1

--- Pseudocode/isomorphic.py
+++ Pseudocode/isomorphic.py
@@ -1,18 +1,4 @@
-def both_terminal(vertex1, vertex2):
-    """Check if the two given BDDs nodes are terminals.
-
-    Arguments:
-
-        [vertex1] The root of the first BDD
-        [vertex2] The root of the second BDD
-
-    Returns: ``bool``
-
-    """
-    return is_terminal(vertex1) and is_terminal(vertex2)
-
-
-def same_type(vertex1, vertex2):
+def is_same_type(vertex1, vertex2):
     """Check if two BDDs have the same type.
 
     Arguments:
@@ -23,8 +9,7 @@ def same_type(vertex1, vertex2):
     Returns: ``bool``
 
     """
-    both_nonterminal = not is_terminal(vertex1) and not is_terminal(vertex2)
-    return both_terminal(vertex1, vertex2) or both_nonterminal
+    return is_terminal(vertex1) == is_terminal(vertex2)
 
 
 def isomorphic(vertex1, vertex2):
